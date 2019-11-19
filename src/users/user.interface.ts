96bab96b8b13705803068b33d50e92c33fc20b1c
zoe <theonlyoneu@daum.net>
2019-11-19 19:35:28 +0900

    [Add] User dto model

---
+++ src/users/user.interface.ts
@@ -0,0 +1,13 @@
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  level: number;
+  coin: number;
+  point: number;
+  health: number;
+  status: boolean;
+  userCode: number;
+}
+
+export default User;
